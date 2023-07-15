0510057cad5b04eda4eff8ecf7e3af1c5f4d4637
Nghia Le <leminhnghia111095@gmail.com>
2023-07-15 16:31:08 +0700

    Fixed done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
- 
-import './App.css';
+import "./App.css";
 import { useWindowSize } from "./useWindowSize";
-import backgroundImage from './image/background.png'
+import backgroundImage from "./image/background.png";
 
 function App() {
   const { size } = useWindowSize({ gameWidth: 1920, gameHeight: 3405 });
-  const { ratio,rotate,marginLeft} = size;         
+  const { rotate, width, height, marginLeft } = size;
+
   return (
-    <div id="root" className="background mobile-rotater " style={{ backgroundImage: `url(${backgroundImage})`, height:3405 * ratio, width: 1920 * ratio, transform: `rotate(${rotate})`,marginLeft:marginLeft }} >
-     
-    </div>
+    <div
+      id="root"
+      className="container-screen mobile-rotater"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        height,
+        width,
+        transform: `rotate(${rotate})`,
+        marginLeft: marginLeft,
+      }}
+    ></div>
   );
 }
 
